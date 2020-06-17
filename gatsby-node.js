613fe136ad298b23fc1cee54e1a56216c1d44c92
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-17 03:35:19 -0700

    pipeline

--- gatsby-node.js
+++ gatsby-node.js
@@ -49,16 +49,16 @@ exports.createPages = ({ actions, graphql }) => {
 };
 
 // Process this site as a theme when installed as a package from npm
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          include: path.dirname(require.resolve('til')),
-          use: [loaders.js()]
-        },
-      ],
-    },
-  });
-};
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//   actions.setWebpackConfig({
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           include: path.dirname(require.resolve('')),
+//           use: [loaders.js()]
+//         },
+//       ],
+//     },
+//   });
+// };
