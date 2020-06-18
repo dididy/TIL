744c4fdd74e7b8e5f482ee3f5c8750cf361cff97
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-17 19:53:03 -0700

    pipeline

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 import React from "react"
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-uoi/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 
 
